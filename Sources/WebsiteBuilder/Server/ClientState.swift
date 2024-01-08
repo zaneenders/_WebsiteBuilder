e36994dc870d724bfb16b42de81755eba232a6dc
zane <39070793+zaneenders@users.noreply.github.com>
2024-01-08 08:44:38 -0700

    introduce ClientState

---
+++ Sources/WebsiteBuilder/Server/ClientState.swift
@@ -0,0 +1,123 @@
+import Foundation
+
+// a setup function
+private func setup(_ block: some Block) -> [(String, HTMLElement)] {
+    let mirror = Mirror(reflecting: block)
+    for (label, value) in mirror.children {
+        let l = "\(label == nil ? "" : label!)"
+        // If there is a state property create a new box to swap out with the orignal
+        if let state = value as? any StateProperty {
+            let bp = state.value as! any BoxProperty
+            // TODO save box somewhere?
+            #warning("Start here!!!")
+            /*
+            TODO move to UserState
+            create a muating node tree to match the BlockGraph.
+            Store the boxes in that graph and use a similar dirty equal to invlaid nodes
+            Create new boxes as nessary
+            keep starting boxes at starting state
+            */
+            oldBox = state.value as! Box<Int>
+            print(
+                Unmanaged.passUnretained(state.value as! AnyObject).toOpaque())
+            let nb = bp.clone()
+            newBox = nb as! Box<Int>
+            print(Unmanaged.passUnretained(nb as! AnyObject).toOpaque())
+            // swap box out
+            state.value = nb
+        }
+    }
+
+    if let base = block as? any BaseBlock {
+        switch base.type {
+        case .text:
+            let text = block as! Text
+            let textDiv = textDiv(text)
+            return [(textDiv.rebuild(text.text), textDiv)]
+        case .button:
+            let button = block as! Button
+            let btnInfo = setupButton(button)
+            return [(btnInfo.rebuild(button.label), btnInfo)]
+        case .tuple:
+            let tuple = block as! TupleBlock
+            // TODO flatten heigharchy correctly
+            return setup(tuple.value.acc) + setup(tuple.value.n)
+        }
+    } else {
+        return setup(block.component)
+    }
+}
+
+extension ServerState {
+    struct ClientState {
+        let userID: String
+        let block: any Block
+
+        // DOM State
+        var actions: [String: () -> Void] = [:]
+        var elements: [String: HTMLElement]
+        var order: [Int: String] = [:]
+
+        init(_ id: String, _ root: some Block) {
+            let copy = root
+            let page = setup(copy)
+            self.block = copy
+            var initHtml = ""
+            var pageComponents: [HTMLElement] = []
+            for (h, c) in page {
+                // c = inital page components
+                pageComponents += [c]
+                initHtml += h
+            }
+            var e: [String: HTMLElement] = [:]
+            var o: [Int: String] = [:]
+            for (i, el) in pageComponents.enumerated() {
+                o[i] = el.id
+                e[el.id] = el
+            }
+            self.order = o
+            self.elements = e
+            // Setup session
+            // - build html
+            self.userID = id
+        }
+
+        mutating func drawBody() -> String {
+            actions = [:]
+            return draw(self.block)
+        }
+
+        mutating func draw(_ block: some Block) -> String {
+            let mirror = Mirror(reflecting: block)
+            for (label, value) in mirror.children {
+                let l = "\(label == nil ? "" : label!)"
+                if let state = value as? any StateProperty {
+                    let b = state.value as! any BoxProperty
+                }
+            }
+            if let base = block as? any BaseBlock {
+                switch base.type {
+                case .text:
+                    let text = block as! Text
+                    return div { text.text }
+                case .button:
+                    let button = block as! Button
+                    let id = "\(UUID())"
+                    let copy = userID
+                    actions[id] = {
+                        // print("\(copy): \(id)")
+                        button.action()
+                    }
+                    return """
+                        <div id=\(id) class="button" >\(button.label)</div>
+                        """
+                case .tuple:
+                    let tuple = block as! TupleBlock
+                    return draw(tuple.value.acc) + draw(tuple.value.n)
+                }
+            } else {
+                return draw(block.component)
+            }
+        }
+    }
+}
--- Sources/WebsiteBuilder/Server/ServerState.swift
+++ Sources/WebsiteBuilder/Server/ServerState.swift
@@ -8,126 +8,6 @@ struct ServerResult: Codable {
 var oldBox: Box<Int>!
 var newBox: Box<Int>!
 
-// a setup function
-func setup(_ block: some Block) -> [(String, HTMLElement)] {
-    let mirror = Mirror(reflecting: block)
-    for (label, value) in mirror.children {
-        let l = "\(label == nil ? "" : label!)"
-        // If there is a state property create a new box to swap out with the orignal
-        if let state = value as? any StateProperty {
-            let bp = state.value as! any BoxProperty
-            // TODO save box somewhere?
-            #warning("Start here!!!")
-            /*
-            TODO move to UserState
-            create a muating node tree to match the BlockGraph.
-            Store the boxes in that graph and use a similar dirty equal to invlaid nodes
-            Create new boxes as nessary
-            keep starting boxes at starting state
-            */
-            oldBox = state.value as! Box<Int>
-            print(
-                Unmanaged.passUnretained(state.value as! AnyObject).toOpaque())
-            let nb = bp.clone()
-            newBox = nb as! Box<Int>
-            print(Unmanaged.passUnretained(nb as! AnyObject).toOpaque())
-            // swap box out
-            state.value = nb
-        }
-    }
-
-    if let base = block as? any BaseBlock {
-        switch base.type {
-        case .text:
-            let text = block as! Text
-            let textDiv = textDiv(text)
-            return [(textDiv.rebuild(text.text), textDiv)]
-        case .button:
-            let button = block as! Button
-            let btnInfo = setupButton(button)
-            return [(btnInfo.rebuild(button.label), btnInfo)]
-        case .tuple:
-            let tuple = block as! TupleBlock
-            // TODO flatten heigharchy correctly
-            return setup(tuple.value.acc) + setup(tuple.value.n)
-        }
-    } else {
-        return setup(block.component)
-    }
-}
-
-struct UserState {
-    let userID: String
-    let block: any Block
-
-    // DOM State
-    var actions: [String: () -> Void] = [:]
-    var elements: [String: HTMLElement]
-    var order: [Int: String] = [:]
-
-    init(_ id: String, _ root: some Block) {
-        let copy = root
-        let page = setup(copy)
-        self.block = copy
-        var initHtml = ""
-        var pageComponents: [HTMLElement] = []
-        for (h, c) in page {
-            // c = inital page components
-            pageComponents += [c]
-            initHtml += h
-        }
-        var e: [String: HTMLElement] = [:]
-        var o: [Int: String] = [:]
-        for (i, el) in pageComponents.enumerated() {
-            o[i] = el.id
-            e[el.id] = el
-        }
-        self.order = o
-        self.elements = e
-        // Setup session
-        // - build html
-        self.userID = id
-    }
-
-    mutating func drawBody() -> String {
-        actions = [:]
-        return draw(self.block)
-    }
-
-    mutating func draw(_ block: some Block) -> String {
-        let mirror = Mirror(reflecting: block)
-        for (label, value) in mirror.children {
-            let l = "\(label == nil ? "" : label!)"
-            if let state = value as? any StateProperty {
-                let b = state.value as! any BoxProperty
-            }
-        }
-        if let base = block as? any BaseBlock {
-            switch base.type {
-            case .text:
-                let text = block as! Text
-                return div { text.text }
-            case .button:
-                let button = block as! Button
-                let id = "\(UUID())"
-                let copy = userID
-                actions[id] = {
-                    // print("\(copy): \(id)")
-                    button.action()
-                }
-                return """
-                    <div id=\(id) class="button" >\(button.label)</div>
-                    """
-            case .tuple:
-                let tuple = block as! TupleBlock
-                return draw(tuple.value.acc) + draw(tuple.value.n)
-            }
-        } else {
-            return draw(block.component)
-        }
-    }
-}
-
 func printBlock(_ block: some Block) {
     let mirror = Mirror(reflecting: block)
     for (label, value) in mirror.children {
@@ -161,17 +41,21 @@ actor ServerState {
 
     let root: any Block
     var content: String = ""
-    var connections: [String: UserState] = [:]
+    var connections: [String: ClientState] = [:]
+
+    func createClientState(_ id: String) -> ClientState {
+        ClientState(id, root)
+    }
 
     func remove(_ id: String) {
         // TODO clean up Client State, Client has disconnected.
     }
 
-    /// Called when the user hits a button or request a state change to the page
+    /// Called when the client hits a button or request a state change to the page
     func update(_ id: String, _ input: String) -> String {
         let out: String
         // Current connection
-        if var userState = connections[id] {
+        if var clientState = connections[id] {
             /*
             Maybe we swap out the boxes before and after applying the action and drawing the body.
             I do wonder how to keep track of which session needs which boxes.
@@ -179,31 +63,26 @@ actor ServerState {
             */
             print(oldBox?.value)
             print(newBox?.value)
-            if let a = userState.actions[input] {
+            if let a = clientState.actions[input] {
                 a()
             }
             print(oldBox?.value)
             print(newBox?.value)
             // TODO handle logic from input
             let data = try? JSONEncoder().encode(
-                ServerResult(html: userState.drawBody(), javascript: serverJS))
+                ServerResult(html: clientState.drawBody(), javascript: serverJS)
+            )
             if data != nil {
                 out = String(data: data!, encoding: .utf8)!
             } else {
                 out = "server error"
             }
-            connections[id] = userState
+            connections[id] = clientState
         } else {
             // Create new state
-            var state = UserState(id, root)
+            var state = createClientState(id)
             printBlock(state.block)
             print("new connection \(id)")
-            // If it's a new connection will there ever be input?
-            /*
-            if let a = state.actions[input] {
-                a()
-            }
-            */
             // TODO handle logic from input
             let data = try? JSONEncoder().encode(
                 ServerResult(html: state.drawBody(), javascript: serverJS))
